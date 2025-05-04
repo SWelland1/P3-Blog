51305681636c20a88eb0b161c69e8417854ff46f
SWelland1 <sarah.welland@hotmail.com>
2025-05-04 13:45:32 +0100

    summernote added

---
+++ blog/migrations/0003_post_updated_on.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_post_excerpt'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='updated_on',
+            field=models.DateTimeField(auto_now=True),
+        ),
+    ]
