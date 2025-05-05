1833465fe0fba44da534a0e4c14cb993083318ae
SWelland1 <sarah.welland@hotmail.com>
2025-05-05 01:23:41 +0100

    .python_version

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Dog.wsgi
+web: gunicorn codestar.wsgi
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,9 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',
+ALLOWED_HOSTS = ['project-3-p-37dee8438a10.herokuapp.com/',
                  '127.0.0.1',]
 
 
